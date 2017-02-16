ec171e1722683d6547f3dbeda0a9f910b7f2e79e
marcsalvadorferre <marcsalvadorferre@S1-21>
2017-02-16 10:27:43 +0100

    Add project files.

---
+++ RandomEnemyGenerator/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+typedef enum EnemyType { zombie, vampire, ghost, witch };
+
+typedef struct Enemy {
+	EnemyType Type;
+	string name;
+	int health;
+};
+
+Enemy CreateRandomEnemy() {
+
+	return;
+}
+
+void main() {
+	
+}
