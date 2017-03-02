343945626e6b69f006acb6edbaf6297358bb516e
YazlaEU <yazlaeu@gmail.com>
2017-03-02 11:25:48 +0100

    Actualitzacio per a casa

--- DynArray/DynArray.cpp
+++ DynArray/DynArray.cpp
@@ -1,5 +1,32 @@
+#include "DynArray.hh"
 
+DynArray::DynArray(void) {
 
-void main() {
+	m_data = new int [ DYN_ARRAY_DEFAULT_SIZE ];
+	m_capacity = DYN_ARRAY_DEFAULT_SIZE;
+	m_size = DYN_ARRAY_DEFAULT_SIZE;
 
+}
+
+DynArray::DynArray(size_t size, const int &value)
+{
+
+}
+
+int* DynArray::begin (void) const
+{
+	return m_data;
+}
+
+int* DynArray::end(void) const
+{
+	return m_data + m_size;
+}
+
+DynArray fill(int *first, int *last, int value)
+{
+	while (first != last) {
+		*first = value;
+		first++;
+	}
 }
